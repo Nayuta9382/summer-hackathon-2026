565073768ed0f80acc7241c13e8b193a596e6b2f
kumagai <n.kumagai.sys23@morijyobi.ac.jp>
2026-08-27 13:47:49 +0900

    fix/ タグの取得処理及びタグの作成処理時のResponseからuserIdを削除 #12 #15
    - タグ取得処理のuserIdをnumberに修正

--- app/src/backend/services/tagService.ts
+++ app/src/backend/services/tagService.ts
@@ -4,28 +4,26 @@ import { AddTagRequestSchema } from '../types/request/tag/addTagRequest';
 import { TagResponse } from '../types/response/tag/tagResponse';
 
 // タグの一覧をユーザIDから取得する
-export async function getTagsByUserId(userId: string) {
+export async function getTagsByUserId(userId: string): Promise<TagResponse[]> {
     // repositoryからタグの一覧を受け取る
     const tags: Tag[] = await selectTagsByUserId(userId);
 
     // レスポンスオブジェクトに詰めなおす(for分とかで詰めなおすのの省略版的なやつ)
-    const tagResponses: TagResponse[] = tags.map((item) => ({ ...item }));
+    const tagResponses: TagResponse[] = tags.map(({ userId, ...rest }) => rest);
 
     return tagResponses;
 }
 
 // タグを登録する処理
-export async function addTag(userId: number, rawBody: unknown): Promise<Tag> {
+export async function addTag(userId: number, rawBody: unknown): Promise<TagResponse> {
     // バリデーション(失敗時はZodErrorが自動でthrowされ、handleApiErrorのzod部分がレスポンスする)
     const { tagName, colorCode } = AddTagRequestSchema.parse(rawBody);
 
     // DBへ登録
     const newTag = await insertTag({ userId, tagName, colorCode });
 
-    const tagResponse: TagResponse = {
-        ...newTag,
-        userId: String(newTag.userId),
-    };
+    // newTagからuserIdを除いてTagResponse形式にする
+    const { userId: _, ...tagResponse } = newTag;
 
     return tagResponse;
 }
